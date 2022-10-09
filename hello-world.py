afaee9e4f9229dd6f022a638f921d7b03d89aa91
suleyman <snfeme5@gmail.com>
2022-10-09 12:59:26 +0200

    py file updated

--- hello-world.py
+++ hello-world.py
@@ -1,18 +1,2 @@
 print('Hello World')
-print("Sokaktayım, kimsesiz bir sokak ortasında \
-Yürüyorum, arkama bakmadan yürüyorum \
-Yolumun karanlığa saplanan noktasında \
-Sanki beni bekleyen bir hayâl görüyorum \
-Bana düşmez can vermek, yumuşak bir kucakta \
-Ben bu kaldırımların emzirdiği çocuğum \
-Aman, sabah olmasın, bu karanlık sokakta \
-Bu karanlık sokakta bitmesin yolculuğum \
-Kara gökler kül rengi, bulutlarla kapanık \
-Evlerin bacasını kolluyor yıldırımlar \
-İn cin uykuda, yalnız iki yoldaş uyanık \
-Biri benim, biri de serseri kaldırımlar \
-Kaldırımlar çilekeş, yalnızların annesi \
-Kaldırımlar, içimde yaşamış bir insandır \
-Kaldırımlar, duyulur ses kesilince sesi \
-Kaldırımlar içimde kıvrılan bir lisandır \
-Kaldırımlar")
+print("Sokaktayım, kimsesiz bir sokak ortasında,Yürüyorum, arkama bakmadan yürüyorum")
